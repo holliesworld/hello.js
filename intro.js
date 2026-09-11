7ba4370479b7341c6f61990679377a62f6c44725
Koyejo Olaoluwa <161835594+holliesworld@users.noreply.github.com>
2026-09-11 03:35:04 +0100

    Update intro.js

--- intro.js
+++ intro.js
@@ -1,1 +1,1 @@
-console.log('Hello World, Olaoluwa is my name, and favourite programming language is Python. Thanks.')
+console.log('Hello World, Olaoluwa is my name, and favourite programming language is Python. Thanks.');
